d9689c8a17fdeada892559ac1d6bd9ea2a0a40e8
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-11-15 01:02:08 -0600

    Add site-specific database configuration in CreateBackupJob
    This update introduces a method to load and set site-specific database configurations dynamically before executing the backup command. Additionally, it modifies the existing backup command to use the loaded configurations, ensuring the backup reflects the correct database settings.

--- src/Jobs/CreateBackupJob.php
+++ src/Jobs/CreateBackupJob.php
@@ -2,11 +2,13 @@
 
 namespace ShuvroRoy\FilamentSpatieLaravelBackup\Jobs;
 
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
 use ShuvroRoy\FilamentSpatieLaravelBackup\Enums\Option;
 use Spatie\Backup\Commands\BackupCommand;
 
@@ -20,19 +22,58 @@ class CreateBackupJob implements ShouldQueue
         protected readonly Option $option = Option::ALL,
         protected readonly ?int $timeout = null,
     ) {
+        $this->onQueue('default');
     }
 
+    /**
+     * @throws Exception
+     */
     public function handle(): void
     {
+        // @TODO
+        //Load from database the site configuration
+
+        // Mock site configuration
+        $site = [
+            'host' => 'mariadb_shaddai',
+            'port' => '3306',
+            'database' => 'shaddai',
+            'username' => 'cswni',
+            'password' => 'cswni',
+        ];
+
+        $this->setupConfigForSite($site);
+
         Artisan::call(BackupCommand::class, [
-            '--only-db' => $this->option === Option::ONLY_DB,
-            '--only-files' => $this->option === Option::ONLY_FILES,
-            '--filename' => match ($this->option) {
-                Option::ALL => null,
-                default => str_replace('_', '-', $this->option->value) .
-                    '-' . date('Y-m-d-H-i-s') . '.zip'
-            },
+            '--only-db' => 1,
+            '--only-files' => 0,
+            '--filename' => $site ['database'] .'-'.date('Y-m-d-H-i-s') . '.zip',
             '--timeout' => $this->timeout,
         ]);
+    }
+
+    public function setupConfigForSite($site): void
+    {
+
+        try {
+            //Clear the config
+            Artisan::call('config:clear');
+
+            Config::set('backup.backup.name', 'demo');
+            Config::set('database.connections.mysql.host', $site['host']);
+            Config::set('database.connections.mysql.port', $site['port']);
+            Config::set('database.connections.mysql.database', $site['database']);
+            Config::set('database.connections.mysql.username', $site['username']);
+            Config::set('database.connections.mysql.password', $site['password']);
+
+            // Not include files; only database
+            Config::set('backup.backup.source.files.include', []);
+
+            //Filename
+            Config::set('backup.backup.destination.filename_prefix', $site['database'] . '_');
+        } catch (Exception $e) {
+            throw new Exception('Error setting up config for site');
+        }
+
     }
 }
